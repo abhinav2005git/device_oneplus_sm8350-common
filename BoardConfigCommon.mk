fc5f39b288143c0bb4adeed727af7326cf1429bb
Jammy555 <prathap.venkata@gmail.com>
2026-03-15 09:02:17 +0530

    sm8350-common: Adjust cpu0 freq to avoid locking at max freq
    -changed min and max freq of cpu0 along with scheduling rated added
    -hook-up libperf sepolicy
    -utilise cgroup.json
    
    Signed-off-by: Jammy555 <prathap.venkata@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -156,6 +156,7 @@ VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 
 # SEPolicy
 include device/lineage/sepolicy/libion/sepolicy.mk
+include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 include hardware/oplus/sepolicy/qti/SEPolicy.mk
 
